2e4ec2f28a626563e691ba032b85899b60e03aff
John Scheider <john_jps@yahoo.com>
2016-05-10 00:10:00 -0500

    added coalesce

--- Source/FormulaAddIns/FormulaAddIns.cpp
+++ Source/FormulaAddIns/FormulaAddIns.cpp
@@ -8,39 +8,61 @@
 
 void SetString(FormulaAddInData *pReturnValue, const wchar_t *pString)
 {
-	size_t nLen = wcslen(pString);
-	wchar_t *pStringRet = (wchar_t *)GlobalAlloc(GMEM_FIXED, (nLen+1)*sizeof(wchar_t));
-	wcscpy(pStringRet, pString);
-	pReturnValue->pVal = pStringRet;
-	pReturnValue->nVarType = 2;
+    size_t nLen = wcslen(pString);
+    wchar_t *pStringRet = (wchar_t *)GlobalAlloc(GMEM_FIXED, (nLen + 1)*sizeof(wchar_t));
+    wcscpy(pStringRet, pString);
+    pReturnValue->pVal = pStringRet;
+    pReturnValue->nVarType = 2;
 }
 
-// this sample takes a variable number of numbers and simply sums them together, returning a number
+// this sample takes a variable number of numbers and returns the average of non-null entries; if all are null, the result is null.
 extern "C" long _declspec(dllexport) _stdcall AverageNonNull(int nNumArgs, FormulaAddInData *pArgs, FormulaAddInData *pReturnValue)
 {
-	double dRet = 0.0;
-	double dCnt = 0.0;
+    double dRet = 0.0;
+    double dCnt = 0.0;
 
-	for (int x=0; x<nNumArgs; ++x)
-	{
-		if (pArgs[x].nVarType!=1)
-		{
-			SetString(pReturnValue, L"Sample_AddNumbers requires only numeric parameters");
-			return 0;
-		}
+    for (int x = 0; x<nNumArgs; ++x)
+    {
+        if (pArgs[x].nVarType != 1)
+        {
+            SetString(pReturnValue, L"Sample_AddNumbers requires only numeric parameters");
+            return 0;
+        }
 
-		if (pArgs[x].isNull==0) {
-			dRet += pArgs[x].dVal;
-			dCnt += 1.0;
-		}
-	}
+        if (pArgs[x].isNull == 0) {
+            dRet += pArgs[x].dVal;
+            dCnt += 1.0;
+        }
+    }
 
-	if (dCnt > 0.0) {
-		pReturnValue->nVarType = 1;
-		pReturnValue->dVal = dRet / dCnt;
-	} else {
-		pReturnValue->nVarType = 2;
-		pReturnValue->dVal = NULL;
-	}
-	return 1;
+    if (dCnt > 0.0) {
+        pReturnValue->nVarType = 1;
+        pReturnValue->dVal = dRet / dCnt;
+    }
+    else {
+        pReturnValue->nVarType = 2;
+        pReturnValue->dVal = NULL;
+    }
+    return 1;
+}
+
+// this sample takes a variable number of values and returns the first non-null entry
+extern "C" long _declspec(dllexport) _stdcall Coalesce(int nNumArgs, FormulaAddInData *pArgs, FormulaAddInData *pReturnValue)
+{
+    double dRet = 0.0;
+    double dCnt = 0.0;
+
+    for (int x = 0; x<nNumArgs; ++x)
+    {
+        if (pArgs[x].isNull == 0) {
+            pReturnValue->nVarType = pArgs[x].nVarType;
+            pReturnValue->dVal = pArgs[x].dVal;
+            pReturnValue->pVal = pArgs[x].pVal;
+            return 1;
+        }
+    }
+    pReturnValue->nVarType = 2;
+    pReturnValue->dVal = NULL;
+    pReturnValue->pVal = NULL;
+    return 1;
 }
